c04288a051c28e01eb7eaf38e24ab4ca553b56a4
ibrahimdevop21 <ibrahimdevop21@gmail.com>
2024-09-30 05:22:17 +0300

    first commit got fingers wet in SQL

---
+++ up.sql
@@ -0,0 +1,3 @@
+CREATE TABLE users (id INTEGER, name TEXT, age INTEGER, is_admin boolean);
+INSERT INTO users (id, name, age, is_admin) values (1, "John Doe", 27, false);
+INSERT INTO users (id, name, age, is_admin) values (2, "Sally rae", 19, true);
